ff581b530ad7d2c967cb8ec25b69ed11d90baa7a
Leo Sun <leosunsl@outlook.com>
2019-06-13 09:54:58 +0800

    ble_app_uart : make adv never stops

--- ble_app_uart/main.c
+++ ble_app_uart/main.c
@@ -435,7 +435,9 @@ static void on_adv_evt(ble_adv_evt_t ble_adv_evt)
             APP_ERROR_CHECK(err_code);
             break;
         case BLE_ADV_EVT_IDLE:
-            sleep_mode_enter();
+            //sleep_mode_enter();
+            /* never stops adv, just resetart it */
+            advertising_start(false);
             break;
         default:
             break;
